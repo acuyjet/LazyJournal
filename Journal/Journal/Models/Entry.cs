c71a04456d56dcea919112d171e7b795c238b408
Allison.Cuyjet <allison@cuyjet.com>
2016-10-25 17:04:45 -0400

    Add EntriesController and Entry Index view, create Entry class

---
+++ Journal/Journal/Models/Entry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Journal.Models
+{
+    public class Entry
+    {
+
+        public Entry()
+        {
+           
+        }
+
+        public int Id { get; set; }
+
+        public string Prompt { get; set; }
+
+        public string Text { get; set; }
+
+    }
+}
