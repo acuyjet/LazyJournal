ee34d13e0ecaf16d959c7bc222ebd313ca70c9b6
Allison.Cuyjet <allison@cuyjet.com>
2016-11-13 20:51:56 -0500

    Add Prompt class

---
+++ Journal/Journal/Models/Prompt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Journal.Models
+{
+    public class Prompt
+    {
+        public int PromptId { get; set; }
+
+        public string PromptText { get; set; }
+    }
+}
