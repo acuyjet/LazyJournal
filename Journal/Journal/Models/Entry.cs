a592e92f6e99858212e00cfd809f00b4e35e5010
Allison.Cuyjet <allison@cuyjet.com>
2016-11-14 21:21:02 -0500

    Edit Prompt and Entry classes

--- Journal/Journal/Models/Entry.cs
+++ Journal/Journal/Models/Entry.cs
@@ -16,7 +16,6 @@ namespace Journal.Models
         public string Title { get; set; }
 
         public string Text { get; set; }
-
     }
 
     public class EntryContext : DbContext
@@ -40,21 +39,21 @@ namespace Journal.Models
                         EntryId = 1,
                         Date = DateTime.Parse("1/1/2016"),
                         Title = "Deep Thoughts About Tacos",
-                        Text = "Every day is taco ipsum tuesday. These tacos are lit 🔥. It’s taco Tuesday Monday. Fish tacos: lettuce or cabbage, pico de gallo, avocado and a sour cream or citrus/mayonnaise sauce, all placed on top of a corn or flour tortilla. Flour or corn tortillas? CARNE ASADA!! 50 cent tacos! I’ll take 30. If you were a taco, would you eat yourself? Add in a few el Pastor with guac and diced onions. Tacos al pastor made with adobada meat. I’ve been following that taco truck around all day. TACOS!!",
+                        Text = "Every day is taco ipsum tuesday. These tacos are lit 🔥. It’s taco Tuesday Monday. Fish tacos: lettuce or cabbage, pico de gallo, avocado and a sour cream or citrus/mayonnaise sauce, all placed on top of a corn or flour tortilla. Flour or corn tortillas? CARNE ASADA!! 50 cent tacos! I’ll take 30. If you were a taco, would you eat yourself? Add in a few el Pastor with guac and diced onions. Tacos al pastor made with adobada meat. I’ve been following that taco truck around all day. TACOS!!"
                     });
                     context.Entries.Add(new Models.Entry()
                     {
                         EntryId = 2,
                         Date = DateTime.Parse("1/2/2016"),
                         Title = "Deep Thoughts About Cats",
-                        Text = "Sniff other cat's butt and hang jaw half open thereafter chew on cable. See owner, run in terror. Who's the baby swat at dog. Lick the other cats destroy couch russian blue for sit in box Gate keepers of hell sit in window and stare ooo, a bird! yum poop on grasses. Lick arm hair catch mouse and gave it as a present, refuse to leave cardboard box but cats go for world domination, scratch at the door then walk away. Stares at human while pushing stuff off a table bleghbleghvomit my furball really tie the room together or purr while eating and walk on car leaving trail of paw prints on hood and windshield yet friends are not food.",
+                        Text = "Sniff other cat's butt and hang jaw half open thereafter chew on cable. See owner, run in terror. Who's the baby swat at dog. Lick the other cats destroy couch russian blue for sit in box Gate keepers of hell sit in window and stare ooo, a bird! yum poop on grasses. Lick arm hair catch mouse and gave it as a present, refuse to leave cardboard box but cats go for world domination, scratch at the door then walk away. Stares at human while pushing stuff off a table bleghbleghvomit my furball really tie the room together or purr while eating and walk on car leaving trail of paw prints on hood and windshield yet friends are not food."
                     });
                     context.Entries.Add(new Models.Entry()
                     {
                         EntryId = 3,
                         Date = DateTime.Parse("1/3/2016"),
                         Title = "Deep Thoughts About Zombies",
-                        Text = "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus.Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror.Nigh tofth eliv ingdead.",
+                        Text = "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus.Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror.Nigh tofth eliv ingdead."
                     });
                 context.SaveChanges();
                 }
--- Journal/Journal/Models/Prompt.cs
+++ Journal/Journal/Models/Prompt.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Journal.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -10,5 +12,26 @@ namespace Journal.Models
         public int PromptId { get; set; }
 
         public string PromptText { get; set; }
+
+        public static void Prompts()
+        {
+            List<Prompt> prompts = new List<Prompt>();
+
+            prompts.Add(new Prompt() { PromptId = 1, PromptText = "What scares you?" });
+            prompts.Add(new Prompt() { PromptId = 2, PromptText = "Dear Past Me . . ." });
+            prompts.Add(new Prompt() { PromptId = 3, PromptText = "Dear Future Me . . ." });
+            prompts.Add(new Prompt() { PromptId = 4, PromptText = "What’s the worst thing you’ve ever done?" });
+            prompts.Add(new Prompt() { PromptId = 5, PromptText = "What are you looking forward to the most?" });
+            prompts.Add(new Prompt() { PromptId = 6, PromptText = "What's something you're good at?" });
+            prompts.Add(new Prompt() { PromptId = 7, PromptText = "What keeps you up at night worrying?" });
+            prompts.Add(new Prompt() { PromptId = 8, PromptText = "Who is someone you miss?" });
+            prompts.Add(new Prompt() { PromptId = 9, PromptText = "What was your most precious childhood possession?" });
+            prompts.Add(new Prompt() { PromptId = 10, PromptText = "What are you grateful for?" });
+            prompts.Add(new Prompt() { PromptId = 11, PromptText = "What's the story behind your name?" });
+            prompts.Add(new Prompt() { PromptId = 12, PromptText = "What's special about your hometown?" });
+            prompts.Add(new Prompt() { PromptId = 13, PromptText = "What are you listening to?" });
+            prompts.Add(new Prompt() { PromptId = 14, PromptText = "What's a career you wish you had pursued?" });
+            prompts.Add(new Prompt() { PromptId = 15, PromptText = "Tell a story about a childhood friend." });
+        }
     }
 }
